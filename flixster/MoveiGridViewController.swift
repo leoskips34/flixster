1776f99847a87cfaf1e900ed4bdd0cc4f054b7f6
leoskips34 <leo.samuelson@gmail.com>
2020-02-05 23:34:05 -0800

    flix

--- flixster/MoveiGridViewController.swift
+++ flixster/MoveiGridViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MoveiGridViewController: UIViewController {
+class MoveiGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
 
-     var movies = [[String:Any]]()
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies = [[String:Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -38,6 +46,24 @@ class MoveiGridViewController: UIViewController {
            }
         }
         task.resume()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let postPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + postPath)!
+        
+        cell.posterView.af_setImage(withURL: posterURL)
+        
+        return cell
     }
     
 
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Leo Samuelson on 2/5/20.
+//  Copyright © 2020 NDNU. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIView!
+}
